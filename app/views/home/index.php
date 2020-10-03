64889666bfcf1e6f48d45ebf2a3eba701efc3364
subastian <subastian.sk@gmail.com>
2020-10-03 10:13:50 +0700

    Menambahkan file yang mengontrol mvc

---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <div class="jumbotron mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya Yang Cukup Sederhana!</h1>
+    <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+    <hr class="my-4">
+    <p>Maaf Ini Website Saya Tidak Selesai Karena Ini Tugas Kuliah</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </div>
+
+
+</div>
